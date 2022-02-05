419edb1c3f24ca26bdffac89cf57b12b92e4e436
Steffen <schuft69@web.de>
2022-02-05 16:53:45 +0100

    fixed check for link

--- install.sh
+++ install.sh
@@ -10,7 +10,10 @@ DOTFILESDIR=~/.dotfiles/dotfiles
 for DOTFILE in $(find $DOTFILESDIR -type f)
 do
   #echo $(basename $DOTFILE)
-  [[ -f ~/$(basename $DOTFILE) ]] && echo "  create backup to of ~/$(basename $DOTFILE) to ~/.dotfilesbackup/$(basename $DOTFILE)" && mv ~/$(basename $DOTFILE) ~/.dotfilesbackup/
+  ([ -f ~/$(basename $DOTFILE) ] && [ ! -L ~/$(basename $DOTFILE) ]) \
+     && echo "  create backup to of ~/$(basename $DOTFILE) to ~/.dotfilesbackup/$(basename $DOTFILE)" \
+     && mv ~/$(basename $DOTFILE) ~/.dotfilesbackup/
+
   echo " "
   echo "  ln -fs ${DOTFILE} ~/"
   ln -s ${DOTFILE} ~/
